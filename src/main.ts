2c935c38de76c074ec5fa07f02896474e055b7d5
claragzv <claragzv@gmail.com>
2025-04-29 21:52:25 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import HeaderTemplate from "./templates/header.html?raw";
 import Footer from "./templates/footer.html?raw";
 import Main from "./templates/main.html?raw";
 import { Character } from "./api/model/api.model";
+import { displayCharacters } from "./modules/characters";
 
 const root = document.querySelector<HTMLDivElement>("#app")!;
 
@@ -24,10 +25,22 @@ characterContent.classList.add("character-content");
 
 main.appendChild(characterContent);
 
+// H1
+const h1 = document.createElement("h1");
+h1.innerText = "Aplicación de Rick y Morty";
+
 // Creo una constante que se llama button, al igual que hemos creado antes
 // header, main y footer
 const button = document.createElement("button");
 // Le meto un texto al botón
 button.innerText = "Cargar personajes";
-//
+// Le meto estilos al botón
+button.classList.add("button");
+// Le meto un evento al botón, que cuando se haga click en
+// consola aparezca "Cargando personajes..."
+button.addEventListener("click", () => {
+  console.log("Cargando personajes...");
+  displayCharacters(characterContent);
+  console.log("Personajes cargados");
+});
 main.appendChild(button);
